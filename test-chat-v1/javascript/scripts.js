7263ea5bf3b5bdc245cfced666c9a3f3614545a2
Rodney St Cloud <hoyrod1@aol.com>
2024-07-13 17:15:47 -0400

    In the scripts.js file created the functionality to request the media device to allow or deny access

--- test-chat-v1/javascript/scripts.js
+++ test-chat-v1/javascript/scripts.js
@@ -1,1 +1,24 @@
 console.log(`//======== THE SCRIPT FILE IS CONNECTED =======//`);
+// The steam variable will store the stream
+let stream = null;
+// This is a object containing the type of media-tracks/hardware
+const constrants = {
+  audio: true,
+  video: true,
+};
+
+// Async functions to capture the media device.
+const getMicAndCamera = async (e) => {
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(constrants);
+    console.log(stream);
+  } catch (error) {
+    // User denied access to media device
+    console.log("The requested constrants was denied", error);
+  }
+};
+
+// Cache the share mic and camera button
+const shareButton = document.getElementById("share");
+// Add event listener to the share mic and camera button
+shareButton.addEventListener("click", (e) => getMicAndCamera(e));
