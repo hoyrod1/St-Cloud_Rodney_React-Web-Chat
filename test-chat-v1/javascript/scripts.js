ef70ee8a906e8ccc018c05522ab5110c6cba6e1c
Rodney St Cloud <hoyrod1@aol.com>
2024-07-14 07:36:21 -0400

    In the scripts.js file created the functionality to show/play the video feed with the audio and video track. Also the funtonality was created to stop the video/audio track/feed

--- test-chat-v1/javascript/scripts.js
+++ test-chat-v1/javascript/scripts.js
@@ -1,4 +1,8 @@
 console.log(`//======== THE SCRIPT FILE IS CONNECTED =======//`);
+
+// Cache the video element
+const videoEl = document.getElementById("my-video");
+
 // The steam variable will store the stream
 let stream = null;
 // This is a object containing the type of media-tracks/hardware
@@ -7,7 +11,7 @@ const constrants = {
   video: true,
 };
 
-// Async functions to capture the media device.
+// Async functions to capture the media device
 const getMicAndCamera = async (e) => {
   try {
     stream = await navigator.mediaDevices.getUserMedia(constrants);
@@ -18,7 +22,38 @@ const getMicAndCamera = async (e) => {
   }
 };
 
+// Async functions to show video feed
+const showMyVideoFeed = async (e) => {
+  // console.log("Sanity Check", e);
+  // console.log(videoEl);
+  // This will set our video stream to our video element
+  videoEl.srcObject = stream;
+  // Cache the media tracks
+  const tracks = stream.getTracks();
+  console.log(tracks);
+};
+
+// Async functions to capture the media device
+const stopMyVideoFeed = async (e) => {
+  // console.log("Sanity Check", e);
+  // Cache the media tracks
+  const tracks = stream.getTracks();
+  tracks.forEach((track) => {
+    // console.log(track);
+    // This stops/disassociates the video & audio track from the source
+    track.stop();
+  });
+};
+
 // Cache the share mic and camera button
-const shareButton = document.getElementById("share");
-// Add event listener to the share mic and camera button
-shareButton.addEventListener("click", (e) => getMicAndCamera(e));
+const shareMicAndCameraButton = document.getElementById("share");
+// Add event listener to the share mic and camera
+shareMicAndCameraButton.addEventListener("click", (e) => getMicAndCamera(e));
+// Cache the show video button
+const showVideoButton = document.getElementById("show-video");
+// Add event listener to show the video
+showVideoButton.addEventListener("click", (e) => showMyVideoFeed(e));
+// Cache the stop video button
+const stopVideoButton = document.getElementById("stop-video");
+// Add event listener to stop the video
+stopVideoButton.addEventListener("click", (e) => stopMyVideoFeed(e));
